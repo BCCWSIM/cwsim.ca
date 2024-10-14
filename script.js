749b8735d00663dba85d2a0d12079d65012289c2
BCCWSIM <167340173+BCCWSIM@users.noreply.github.com>
2024-10-13 18:15:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let headers, indices = {};
 
     // Fetch and parse CSV data
-    fetch('Resources.csv')
+    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTLwQLbqL0fuco7nsIr7wLUBwvj9XWAy0QAG6wN8SJ2rpqWGCseG5uMtMVqCnDZBNQLAW56uk8siPP1/pub?output=csv')
         .then(response => response.text())
         .then(csvData => {
             items = parseCSV(csvData);
